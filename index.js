c49cda9c8bd7a79be2a9b536b34b9f826ca881dd
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2022-12-02 10:04:28 -0700

    started writing the functions in the index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const inquirer = require("inquirer");
 const mysql = require("mysql2");
 const consoleTable = require("console.table");
 
+// instantiates a connection to sql database
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
@@ -9,6 +10,26 @@ const db = mysql.createConnection({
   database: "employee_tracker",
 });
 console.log(db);
+
+// 
+db.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
+  initalize()
+});
+
+// Display a Welcome Message with ascii art
+console.table("-----Welcome to Employee Tracker-----");
+
+
+// Use inquirer to ask initial questions. View departments, view roles, view employees, add department, add role, add employee, update employee.
+const initalize = async () => {
+  
+}
+
+// Use switch statements to call view/add functions depending on user selection
+
+// Make functions for each of the 7 choices.
 
 db.query("SELECT * FROM department", function (err, results) {
   console.table(results);
