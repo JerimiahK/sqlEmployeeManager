c1fef70a70cad6f6e9264f7365c330c0ce77210e
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2022-12-01 22:55:09 -0700

    added the tables to the schema.sql file

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const inquirer = require("inquirer");
-const mysql = require("mysql");
+const mysql = require("mysql2");
+const consoleTable = require("console.table");
 
-const db = mysql.createConnection(
-  {
-    host: "localhost",
-    user: "root",
-    password: "R3nn@srequest",
-    database: "employee_tracker",
-  },
-  console.log(`Connected to the classlist_db database.`)
-);
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "R3nn@srequest",
+  database: "employee_tracker",
+});
+console.log(db);
+
+db.query("SELECT * FROM roles", function (err, results) {
+  console.log(results);
+});
